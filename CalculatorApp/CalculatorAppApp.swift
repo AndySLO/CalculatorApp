03cf5f4f108407fabace29c9b1cd6df02c0ce70b
Andrey Padalka <andrey.padalka@gmail.com>
2021-09-24 23:39:27 +0300

    Initial Commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by Andrey Padalka on 9/24/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
